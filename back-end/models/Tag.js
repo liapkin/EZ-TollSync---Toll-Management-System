53b4382579c1b97573cd316f822c951417b680a0
ntua-el20803 <el20803@mail.ntua.gr>
2025-01-19 18:14:38 +0200

    Update Tag.js

--- back-end/models/Tag.js
+++ back-end/models/Tag.js
@@ -3,7 +3,8 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     return sequelize.define('Tag', {
         id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-        providerId: { type: DataTypes.INTEGER, allowNull: false },
+        tagRef: { type: DataTypes.STRING, allowNull: false },
+        operatorId: { type: DataTypes.INTEGER, allowNull: false },
     }, {
         tableName: 'tag',
         timestamps: false,
