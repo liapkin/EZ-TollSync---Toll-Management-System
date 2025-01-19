f272faee63b55cd1fe9e79817141cb12335813eb
ntua-el20803 <el20803@mail.ntua.gr>
2025-01-19 18:14:19 +0200

    Update Pass.js

--- back-end/models/Pass.js
+++ back-end/models/Pass.js
@@ -3,10 +3,10 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     return sequelize.define('Pass', {
         id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-        timestamp: { type: DataTypes.INTEGER, allowNull: false },
-        charge: { type: DataTypes.INTEGER, allowNull: false },
-        tagId: { type: DataTypes.INTEGER, allowNull: false },
-        tollStationId: { type: DataTypes.INTEGER, allowNull: false },
+        timestamp: { type: DataTypes.STRING, allowNull: true },
+        charge: { type: DataTypes.REAL, allowNull: false },
+        tagId: { type: DataTypes.INTEGER, allowNull: true },
+        tollStationId: { type: DataTypes.INTEGER, allowNull: true },
     }, {
         tableName: 'pass',
         timestamps: false,
