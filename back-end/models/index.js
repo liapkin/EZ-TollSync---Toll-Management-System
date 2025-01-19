6f0c103eab166eeee650658a06b0c8fa22a294d3
ntua-el20803 <el20803@mail.ntua.gr>
2025-01-19 18:13:40 +0200

    Update and rename index - n.js to index.js

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -20,13 +20,23 @@ const models = {
 models.Operator.hasMany(models.TollStation, { foreignKey: 'operatorId' });
 models.TollStation.belongsTo(models.Operator, { foreignKey: 'operatorId' });
 
-models.Operator.hasMany(models.Tag, { foreignKey: 'providerId' });
-models.Tag.belongsTo(models.Operator, { foreignKey: 'providerId' });
+models.Operator.hasMany(models.Tag, { foreignKey: 'operatorId' });
+models.Tag.belongsTo(models.Operator, { foreignKey: 'operatorId' });
 
 models.Tag.hasMany(models.Pass, { foreignKey: 'tagId' });
 models.Pass.belongsTo(models.Tag, { foreignKey: 'tagId' });
 
 models.TollStation.hasMany(models.Pass, { foreignKey: 'tollStationId' });
 models.Pass.belongsTo(models.TollStation, { foreignKey: 'tollStationId' });
+
+
+// models.Operator.hasMany(models.Tag, { foreignKey: 'providerId' });
+// models.Tag.belongsTo(models.Operator, { foreignKey: 'providerId' });
+
+// models.Tag.hasMany(models.Pass, { foreignKey: 'tagId' });
+// models.Pass.belongsTo(models.Tag, { foreignKey: 'tagId' });
+
+// models.TollStation.hasMany(models.Pass, { foreignKey: 'tollStationId' });
+// models.Pass.belongsTo(models.TollStation, { foreignKey: 'tollStationId' });
 
 module.exports = { sequelize, models };
